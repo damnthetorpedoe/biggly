9a28ff25c22e497732007e83370f398f875f28e2
damnthetorpedoe <68748475+damnthetorpedoe@users.noreply.github.com>
2020-09-23 10:18:30 -0700

    Add files via upload

---
+++ Arrays/src/Demo1.java
@@ -0,0 +1,4 @@
+import java.util.Scanner;
+public class Demo1 {
+
+	public static void main(String[] args){	 		String[] emp_id = new String[100]; 		for (int i = 0; i<100; i++){ 			int idnum = i+100; 			emp_id[i] = "ID#"+idnum; 			System.out.println(emp_id[i]); 		} 		} 		 	} 
